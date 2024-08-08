80c89d2c6268919c3ef3e377112a0d742ac3228f
SinxLinonZ <urara.kbys@gmail.com>
2024-08-08 22:06:13 +0900

    Update: eagle-twitter-save shift to bulk save

--- eagle-twitter-save.js
+++ eagle-twitter-save.js
@@ -18,53 +18,82 @@
 
 GM_addStyle(GM_getResourceText('Materialize_css'));
 
-(function() {
+function getImageInfo(element) {
+    const imgElement = element;
+    const articleElement = imgElement.closest("article");
+
+    const imgData = {
+        src: imgElement.src.replace(/name=(.*)/, "name=orig").replace("format=webp", "format=jpg"),
+        tweetLink: imgElement.closest('a').href.replace(/\/photo\/\d+$/, ''),
+        tweetUserId: '@' + articleElement.querySelector('a[href^="/"]').href.split('/').pop(),
+        tweetText: articleElement.querySelector('div[dir="auto"]')?.textContent || '',
+        tweetTime: Math.floor(new Date(articleElement.querySelector('time').dateTime).getTime()),
+    }
+
+    console.table(imgData);
+    return imgData;
+}
+
+function getArticleImages(element) {
+    if (element.length == 0) return;
+
+    const imgElement = element[0];
+    const articleElement = imgElement.closest("article");
+
+    const imgDataList = [...articleElement.querySelectorAll('a[href*="/photo/"] img')].map(img => {
+        return getImageInfo(img);
+    });
+
+    return imgDataList;
+}
+
+function saveImage(imgData) {
+    GM_xmlhttpRequest({
+        url: 'http://localhost:41595/api/item/addFromURL',
+        method: "POST",
+        data: JSON.stringify({
+            url: imgData.src,
+            name: imgData.tweetUserId + ' ' + imgData.tweetText,
+            annotation: imgData.tweetUserId + '\r\n' + imgData.tweetText,
+            website: imgData.tweetLink,
+            modificationTime: imgData.tweetTime,
+        }),
+        onload: function (response) {
+            if (response.status !== 200) {
+                console.error('Eagle Save Error', response);
+                M.toast({ html: `<span style="color: red; font-weight: bold;">Eagle Save Error: ${imgData.tweetUserId}</span>` });
+                return;
+            }
+            M.toast({ html: `<span style="color: green; font-weight: bold;">Saved: <span style="color: white">${imgData.tweetUserId}</span></span>` });
+        }
+    });
+}
+
+(function () {
     'use strict';
 
     console.log('Eagle Twitter Save Start');
     M.AutoInit();
-    
-    $('body').on('contextmenu',  function(e) {
+
+    $('body').on('keydown', function (e) {
+        if (!e.shiftKey) {
+            return;
+        }
+        e.preventDefault();
+
+        const imgData = getArticleImages(document.querySelectorAll('a:hover'));
+        imgData.forEach(imgData => {
+            saveImage(imgData);
+        });
+    });
+
+    $('body').on('contextmenu', function (e) {
         if (!e.ctrlKey) {
             return;
         }
-
         e.preventDefault();
 
-        const imgElement = e.target;
-        const articleElement = imgElement.closest("article");
-
-        const imgData = {
-            src: imgElement.src.replace(/name=(.*)/, "name=orig").replace("format=webp", "format=jpg"),
-            tweetLink: imgElement.closest('a').href.replace(/\/photo\/\d+$/, ''),
-            tweetUserId: '@' + articleElement.querySelector('a[href^="/"]').href.split('/').pop(),
-            tweetText: articleElement.querySelector('div[dir="auto"]')?.textContent || '',
-            tweetTime: Math.floor(new Date(articleElement.querySelector('time').dateTime).getTime()),
-        }
-
-        console.log(articleElement);
-        console.table(imgData);
-
-        GM_xmlhttpRequest({
-            url: 'http://localhost:41595/api/item/addFromURL',
-            method: "POST",
-            data: JSON.stringify({
-                url: imgData.src,
-                name: imgData.tweetUserId + '\r\n' + imgData.tweetText,
-                website: imgData.tweetLink,
-                modificationTime: imgData.tweetTime,
-            }),
-            onload: function(response) {
-
-                if (response.status !== 200) {
-                    console.error('Eagle Save Error', response);
-                    M.toast({html: `<span style="color: red; font-weight: bold;">Eagle Save Error: ${imgData.tweetUserId}</span>`});
-                    return;
-                }
-
-                M.toast({html: `<span style="color: green; font-weight: bold;">Saved: <span style="color: white">${imgData.tweetUserId}</span></span>`});
-            }
-        });
-        
+        const imgData = getImageInfo(e.target);
+        saveImage(imgData);
     });
 })();
